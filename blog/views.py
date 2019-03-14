f5af6304e597b6bdc44755adbcfa50c80537f9dc
mehmethizmali <mehmethizmali@gmail.com>
2019-03-14 10:58:34 +0300

    Sitede gönderi oluşturma ve düzenleme için view'ler eklendi

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 
 
@@ -14,3 +16,46 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+#veri Ekleme
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+          #Post veri tabanına kayıt etme(veri ekleme) işlemi yapıyoruz
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            #Veri ekleme işleminden sonra 'post_detail sayfasına yönlendiriyoruz'
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'myForm': form})
+
+
+#veri güncelleme
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    #POST metodu ile pk(primary key) ile ilgili veri alınıyor
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        #Alınan veri Post tablosunda güncelleniyor
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            #veri güncelleme işleminden sonra 'post_detail sayfasına yönlendiriyoruz'
+             # return redirect('post_list')
+            return redirect('post_detail', pk=post.pk)
+   #Veri güncellenmediğine 'blog/post_edit.html' sayfasını render ediyoruz
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'myForm': form})    
+    
+
+	
+
+
